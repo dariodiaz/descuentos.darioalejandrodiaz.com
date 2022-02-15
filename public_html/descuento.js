63ae666e1daee59e5d7f4e6488d525e55476fd77
Dario Diaz <dario.dad@gmail.com>
2022-02-15 13:40:19 -0300

    Add more input validations

--- public_html/descuento.js
+++ public_html/descuento.js
@@ -9,11 +9,16 @@ function onClickBotonParaCalcularDescuento() {
 	const inputPrecio = document.getElementById('precio');
 	const inputPorcentajeDeDescuento = document.getElementById('porcentaje-descuento');
 	const resultado = document.getElementById('resultado');
-	if(!inputPrecio.value || !inputPorcentajeDeDescuento.value) {
+	debugger;
+	if (isValidData(inputPrecio.value, inputPorcentajeDeDescuento.value)) {
 		resultado.innerHTML = "<p style='color: red;'>"+
 		"Por favor ingresa todos los datos</p>"
 		return;
 	}
 	const precioConDescuento = calcularPrecioConDescuento(inputPrecio.value, inputPorcentajeDeDescuento.value);
 	resultado.innerText = "El precio con descuento es: " + precioConDescuento;
+}
+
+function isValidData(precio, porcentaje) {
+	return (!precio || !porcentaje) || (precio < 0 || porcentaje < 0);
 }
